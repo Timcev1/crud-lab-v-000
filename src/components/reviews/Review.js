b5e8f629619d79cd44bed776038a47aaf2240df4
Timothy Cevallos <timcev@gmail.com>
2017-10-18 16:37:07 +0000

    automatically backed up by learn

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+  handleOnClick = () => {
+    this.props.store.dispatch({
+      type: 'DELETE_REVIEW',
+      review: this.props.review,
+      id: this.props.review.id,
+      restaurantId: this.props.restaurantId
+    });
+  }
+  
   render() {
     return (
       <li>
+        {this.props.review.text}
+        <button onClick={this.handleOnClick}/>
       </li>
     );
   }
