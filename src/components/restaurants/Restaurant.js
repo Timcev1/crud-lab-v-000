ede53eb8abbff986b1c3d39df5c85253027271d3
Timothy Cevallos <timcev@gmail.com>
2017-10-18 16:25:05 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -5,6 +5,11 @@ class Restaurant extends Component {
   render() {
     return (
       <div>
+        <li>
+          {this.props.restaurant.text}
+          <ReviewInput store={this.props.store} restaurantId={this.props.restaurant.id}/>
+          <button onClick={this.handleOnClick}/>
+        </li>
       </div>
     );
   }
